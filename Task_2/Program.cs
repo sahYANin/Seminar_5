fcdcbbee4cf20ebe98307bd7aa25d2c7f90ea61c
XFX <sah0329@mail.ru>
2022-07-10 22:09:38 +0500

    коммит

---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Задача 36: 
+    Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+*/
+int pi, n = 0, s = 0;
+Console.Write("Введите количество элементов в массиве: ");
+pi = Convert.ToInt32(Console.ReadLine());
+int[] Num = new int[pi];
+
+Console.Write("Сумма чётных(2) или нечётных(1)?: ");
+n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0) n = 1;
+else n = 0;
+
+// Console.WriteLine(n);
+for (int i = 0; i < pi; i++)
+{
+    Num[i] = new Random().Next(-100, 1001);
+}
+Console.WriteLine("[" + string.Join(",", Num) + "]");
+
+for (; n < pi; n++)
+{
+    s = s + Num[n];
+    n++;
+}
+
+Console.Write("Сумма: ");
+Console.WriteLine(s);
---
+++ Task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+    Задача 38: 
+    Задайте массив вещественных чисел. Найдите разницу между 
+максимальным и минимальным элементов массива.
+*/
+
+int mi=1001,mx=-100,pi;
+
+Console.Write("Введите количество элементов в массиве: ");
+pi = Convert.ToInt32(Console.ReadLine());
+int[] Num = new int[pi];
+
+for (int i = 0; i < pi; i++)
+{
+    Num[i] = new Random().Next(-100, 1001);
+    if(mx<Num[i])mx=Num[i];
+    if(mi>Num[i])mi=Num[i];
+    // Console.WriteLine(mi);
+}
+Console.WriteLine("[" + string.Join(",", Num) + "]");
+
+Console.Write("Разница между максимальным и минимальным: ");
+Console.WriteLine(mx-mi);
+
+
+// Console.WriteLine(mx);
+// Console.WriteLine(mi);
